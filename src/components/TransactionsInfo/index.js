3705af65e7647f6f65b65e79488a8c552f8cd6c5
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-01-27 17:14:14 -0100

    feat: making fetch with axios and passing the data of the transaction by props

---
+++ src/components/TransactionsInfo/index.js
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect, useState } from "react"
+import Transaction from "../Transaction"
+import { BigBox, TotalWrapper, TransactionsWrapper } from "./style"
+
+import axios from 'axios'
+
+const TransactionsInfo = () => {
+
+    const [transactions, setTransactions] = useState([])
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+          axios.get('http://localhost:3000/api/transactions').then(response => {
+            const { data } = response.data
+            setTransactions(data)
+          })
+        };
+    
+        fetchData(); 
+    }, []);
+
+
+
+
+    return(
+
+        <>
+            <BigBox>
+                <TransactionsWrapper>
+                    <h4>Transações:</h4>
+
+                    {
+                        transactions.map(transaction => (
+                            <Transaction
+                                key={transaction._id}
+                                name={transaction.name}
+                                value={transaction.value}
+                                description={transaction.description}
+                            />
+                        ))
+                    }
+                    <Transaction />
+
+                </TransactionsWrapper>
+                <TotalWrapper>
+                    <h4>Total Mensal:</h4>
+                    <h3>2,950,00 €</h3>
+                </TotalWrapper>
+            </BigBox>
+        </>
+    )
+}
+
+export default TransactionsInfo
